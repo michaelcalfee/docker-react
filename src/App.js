e58a29794b6f18e4760482ed52e240a03455fc5d
Michael Calfee <michael.calfee@frogdesign.com>
2019-05-29 11:23:49 -0500

    Updated text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there2!
+          Hi there on AWS!
         </a>
       </header>
     </div>
